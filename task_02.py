bb3fc6d23c06e35c303371cf252fa4a9dfccd462
igor-nn <masterhand@inbox.ru>
2022-10-30 14:46:13 +0300

    task_02 final

--- task_02.py
+++ task_02.py
@@ -9,3 +9,23 @@
 
 # Выведите на консоль общий размер населения перечисленных городов как сумму населения всех городов
 # Итого размер населения - ХХ человек
+
+
+city = ['Moscow', 'Nizhny Novgorod', 'St. Petersburg', 'Volgograd']
+#city = [['Moscow',12000], ['Nizhny Novgorod',2300], ['St. Petersburg',5300], ['Volgograd',1000]]
+#print(city)
+population = [12000,1300,5300,1000]
+#population = [[city[0],12000],[city[1],1300],[city[2],5300],[city[3],1000] ]
+# #print(population)
+size = [
+        [city[0]]+[population[0]],
+        [city[1]]+[population[1]],
+        [city[2]]+[population[2]],
+        [city[3]]+[population[3]]
+        ]
+
+#result = f"{city}\n{population}\n{size}"
+#print(result)
+print("Население",city[1],"-",population[1],"человек.")
+x = population[0]+population[1]+population[2]+population[3]
+print("Итого размер населения:",x,"человек.")

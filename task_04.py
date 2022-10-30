1c2ba647f99fdad8467bb9082776538ac93b7d57
igor-nn <masterhand@inbox.ru>
2022-10-30 16:17:15 +0300

    task_04

--- task_04.py
+++ task_04.py
@@ -37,4 +37,4 @@ if z == x or z == y:
 
 print("Треки:",x+1,'+',y+1,'+',z+1)
 result_time = my_favorite_songs[x][1]+my_favorite_songs[y][1]+my_favorite_songs[z][1]
-print(result_time)
+print("Три песни звучат:",result_time, "минут")

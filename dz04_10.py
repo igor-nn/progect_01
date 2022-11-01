3eb06470836ccf701e34002e428043616c9f6277
igor-nn <masterhand@inbox.ru>
2022-11-01 23:46:48 +0300

    индексация

--- dz04_10.py
+++ dz04_10.py
@@ -7,10 +7,10 @@ my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start M
 
 #from re import X
 
-print(my_favorite_songs[:14],  end=' ' )
+print(my_favorite_songs[:14],  end='   ' )
 #print(my_favorite_songs.find('N'))
 #print(my_favorite_songs[64:], end=' ')
-print(my_favorite_songs[-13:], end=' ')
-print(my_favorite_songs[16:30], end = ' ')
+print(my_favorite_songs[-13:], end='   ')
+print(my_favorite_songs[16:30], end = '   ')
 #print(my_favorite_songs[51:62])
 print(my_favorite_songs[51:-15])

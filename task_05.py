8824241a67a855555472546e3cac8f07d2c7b3c9
igor-nn <masterhand@inbox.ru>
2022-11-02 15:28:30 +0300

    сколько денег взять в долг

--- task_05.py
+++ task_05.py
@@ -12,11 +12,11 @@ salary, expenses = 10000, 12000
 #for expenses in 11:
 x = 1
 dept = expenses - salary
-while x<=11:
+while x <= 11:
 
     expenses += (expenses*3/100)
     dept += expenses - salary
-    x+=1
+    x += 1
     #print(int(expenses))
 
 print("Необходимо взять в дог",round(dept,2), "рублей")
